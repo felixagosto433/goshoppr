4b2002aa9db9e0c7595c2bc8bf4feefdfc34518a
felix-i4w <felix@impulse4women.org>
2024-12-19 02:06:53 +0100

    Integrated weaviate cloud to heroku

--- app/client.py
+++ app/client.py
@@ -1,3 +1,22 @@
 import weaviate
+import os
+from dotenv import load_dotenv
+from weaviate.classes.init import AdditionalConfig, Timeout, Auth
 
-client = weaviate.connect_to_local()
+load_dotenv()
+
+WEAVIATE_ADMIN_KEY = os.getenv("WEAVIATE_ADMIN_KEY")
+
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url="https://e9crkmaxsxea5xwram0vkw.c0.europe-west3.gcp.weaviate.cloud",
+    auth_credentials=Auth.api_key(WEAVIATE_ADMIN_KEY),
+    additional_config=AdditionalConfig(timeout=Timeout(init=10)),
+)
+
+# Test connection
+if client.is_ready():
+    print("Connected to Weaviate Cloud Service!")
+else:
+    print("Failed to connect.")
+
+client.close()
