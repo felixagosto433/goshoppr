2f4bf2b18e9c1123101d5bc0679a0365f0c90dfe
felix-i4w <felix@impulse4women.org>
2024-12-19 02:06:53 +0100

    Integrated weaviate cloud to heroku

--- flask_weaviate_testing_request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/search"
-data = {"query": "Cartílago de Tiburón"}
-headers = {"Content-Type": "application/json"}
-
-response = requests.post(url, json=data, headers=headers)
-print(response.json())
