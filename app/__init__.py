7b864def3640e29ee97435a3729deb110c46fa4d
felix-i4w <felix@impulse4women.org>
2025-01-16 17:53:48 +0100

    Final changes before deploying app in cloud.

--- app/__init__.py
+++ app/__init__.py
@@ -1,50 +1,19 @@
-import os
-import weaviate
-from weaviate.classes.init import Auth
 from flask import Flask
-import logging
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-from config import config
-import os
-
-load_dotenv()
+from .client import client
 
 db = SQLAlchemy()
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-if os.getenv("WEAVIATE_CLOUD_URL") and os.getenv("WEAVIATE_ADMIN_KEY"):
-    client = weaviate.connect_to_weaviate_cloud(
-        cluster_url=os.getenv("WEAVIATE_CLOUD_URL"),
-        auth_credentials=Auth.api_key(os.getenv("WEAVIATE_ADMIN_KEY")),
-    )
-    logger.info("Connected to Weaviate Cloud.")
-
-elif os.getenv("WEAVIATE_LOCAL_URL"):
-    client = weaviate.connect_to_local()
-    logger.info("Connected to Local Weaviate.")
-    
-else:
-    logger.error("Weaviate configuration is missing.")
-    raise ValueError("Weaviate configuration is missing. Please set the appropriate environment variables.")
 
 def create_app(config_class=None):
     app = Flask(__name__)
 
-    # Apply the configuration if provided
-    env = os.getenv('FLASK_ENV', 'development')  # Default to 'development'
-    app.config.from_object(config.get(env, config['development']))
-
+    # Apply configuration
     if config_class:
         app.config.from_object(config_class)
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'  # Default config
 
     db.init_app(app)
 
-    # Import blueprints
+    # Import and register blueprints
     from .routes import main
     app.register_blueprint(main)
 
