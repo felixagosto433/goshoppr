82061013d0a53ce2f64aa66896f465a2670747d8
felix-i4w <felix@impulse4women.org>
2025-01-23 03:40:42 +0100

    Eliminated SQLAlchemy from config.py

--- config.py
+++ config.py
@@ -9,39 +9,34 @@ class Config:
     DEBUG = False
     TESTING = False
 
-    # Database configurations
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     # Weaviate configurations (common fallback values can be specified here)
     WEAVIATE_LOCAL_URL = os.getenv('WEAVIATE_LOCAL_URL', 'http://localhost:8080')  # Default for local
     WEAVIATE_CLOUD_URL = os.getenv('WEAVIATE_CLOUD_URL')  # To be fetched from environment variables
     WEAVIATE_ADMIN_KEY = os.getenv('WEAVIATE_ADMIN_KEY')
+
 
 class DevelopmentConfig(Config):
     """
     Configuration for development environment.
     """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL', 'sqlite:///dev.db')  # Local SQLite database for dev
+
 
 class TestingConfig(Config):
     """
     Configuration for testing environment.
     """
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL', 'sqlite:///test.db')  # Separate DB for tests
+
 
 class ProductionConfig(Config):
     """
     Configuration for production environment.
     """
     DEBUG = False
-    # Other production settings
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///production.db'  # Dummy databse uri for now. 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # Production database (PostgreSQL, etc.)
     WEAVIATE_CLOUD_URL = os.getenv('WEAVIATE_CLOUD_URL')  # Ensure this is set in production
     WEAVIATE_ADMIN_KEY = os.getenv('WEAVIATE_ADMIN_KEY')  # Ensure this is set in production
+
 
 # Mapping configurations to environments
 config = {
