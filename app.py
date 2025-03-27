d47cb0c33fe45c2955413922b2177f943854961d
felix-i4w <felix@impulse4women.org>
2025-03-27 19:47:27 +0100

    Force push updated routes for blueprint registration

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from app import create_app
-from flask_cors import CORS
+
 
 app = create_app()
 print("✅ App created with blueprint + weaviate")
-CORS(app, origins=["https://bananos.mybigcommerce.com"])
+
 
