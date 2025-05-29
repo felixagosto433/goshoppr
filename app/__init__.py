62c8909d3b0cbce3bfd1dab8664b6ad07f3fab56
felix-i4w <felix@impulse4women.org>
2025-05-29 23:48:15 +0200

    Fixing CORS error

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,10 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
 
-    # 🔧 Apply full CORS config
-    #CORS(app, resources={r"/*": {"origins": "https://bananos.mybigcommerce.com"}}, 
-    #     methods=["GET", "POST", "OPTIONS"],
-    #     allow_headers=["Content-Type"])
-    CORS(app)
+    # ✅ Only allow BigCommerce origin
+    CORS(app, resources={r"/*": {"origins": "https://goshop.mybigcommerce.com"}},
+         methods=["GET", "POST", "OPTIONS"],
+         allow_headers=["Content-Type"])
 
     app.register_blueprint(main)
     app.weaviate_client = get_weaviate_client()
