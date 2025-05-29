6b9d4f96210f04bbaf102414902f13fba59ddec9
felix-i4w <felix@impulse4women.org>
2025-05-29 23:58:54 +0200

    Debuging app

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,11 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
 
-    # ✅ Only allow BigCommerce origin
-    CORS(app, resources={r"/*": {"origins": "https://goshop.mybigcommerce.com"}},
-         methods=["GET", "POST", "OPTIONS"],
-         allow_headers=["Content-Type"])
+    # 🔧 Apply full CORS config
+    #CORS(app, resources={r"/*": {"origins": "https://bananos.mybigcommerce.com"}}, 
+    #     methods=["GET", "POST", "OPTIONS"],
+    #     allow_headers=["Content-Type"])
+    CORS(app)
 
     app.register_blueprint(main)
     app.weaviate_client = get_weaviate_client()
